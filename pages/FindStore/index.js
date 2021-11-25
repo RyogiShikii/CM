46e8627e5f0450da61a8135eb7d054ace2300221
Daniel <guosongraiden@gmail.com>
2021-11-24 16:30:37 -0800

    add map component

--- pages/FindStore/index.js
+++ pages/FindStore/index.js
@@ -1,12 +1,14 @@
 import styles from './index.module.css';
 import Inform from '../../components/pandemicInfo/Inform';
 import TextBanner from '../../components/textBanner/textBanner';
+import GoogleMap from '../../components/map/googleMap';
 
 const findStore = () => {
     return (
         <div>
             <Inform />
             <TextBanner />
+            <GoogleMap />
         </div>
     )
 }
